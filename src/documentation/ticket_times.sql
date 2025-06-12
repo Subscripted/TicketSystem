ffbb3c9965a13a868e3103402cd7b2da2d15ecb9
Subscripted <113248538+Subscripted@users.noreply.github.com>
2025-06-12 07:55:25 +0200

    Update ticket_times.sql

--- src/documentation/ticket_times.sql
+++ src/documentation/ticket_times.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS ticket_times (
     id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
     ticket_id          BIGINT NOT NULL,
     ticket_user_id     BIGINT NOT NULL,
-    time               INT NOT NULL DEFAULT 0,         -- z. B. Minuten
+    time               INT NOT NULL DEFAULT 0,
     date_insert        DATETIME NOT NULL,
     date_last_updated  DATETIME NOT NULL,
 
