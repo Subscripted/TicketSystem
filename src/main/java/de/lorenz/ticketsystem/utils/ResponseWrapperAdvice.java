50f5f92c5f3481c605b2a1d77068778f7f6b2259
Lorenz <lor.els@gmx.de>
2025-06-17 21:10:56 +0200

    feat(wrapper): added ResponseWrapperAdvice fro sending the correct Http Response Code

---
+++ src/main/java/de/lorenz/ticketsystem/utils/ResponseWrapperAdvice.java
@@ -0,0 +1,38 @@
+package de.lorenz.ticketsystem.utils;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ResponseWrapperAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return ResponseWrapper.class.isAssignableFrom(returnType.getParameterType());
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType,
+            org.springframework.http.server.ServerHttpRequest request,
+            org.springframework.http.server.ServerHttpResponse response) {
+
+        if (body instanceof ResponseWrapper<?> wrapper) {
+            if (response instanceof ServletServerHttpResponse httpResponse) {
+                httpResponse.setStatusCode(org.springframework.http.HttpStatus.valueOf(wrapper.statusCode()));
+            }
+        }
+
+        return body;
+    }
+}
