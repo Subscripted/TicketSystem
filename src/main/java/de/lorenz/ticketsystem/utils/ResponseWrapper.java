bc32e54d21236fce9a90aa601fc512507a307616
Lorenz <lor.els@gmx.de>
2025-06-26 10:22:30 +0200

    refactor: restructure responseWrapper code without changing behavior

--- src/main/java/de/lorenz/ticketsystem/utils/ResponseWrapper.java
+++ src/main/java/de/lorenz/ticketsystem/utils/ResponseWrapper.java
@@ -2,50 +2,50 @@ package de.lorenz.ticketsystem.utils;
 
 import de.lorenz.ticketsystem.globals.GlobalHttpStatusCode;
 
-public record ResponseWrapper<T>(String message, T response, int statusCode) {
+public record ResponseWrapper<T>(String message, int statusCode, T response) {
 
     public static <T> ResponseWrapper<T> ok(T data, String message) {
-        return new ResponseWrapper<>(message, data, GlobalHttpStatusCode.OK.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.OK.getCode(), data);
     }
 
     public static <T> ResponseWrapper<T> error(String message) {
-        return new ResponseWrapper<>(message, null, GlobalHttpStatusCode.INTERNAL_ERROR.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.INTERNAL_ERROR.getCode(), null);
     }
 
     public static <T> ResponseWrapper<T> error(T data, String message) {
-        return new ResponseWrapper<>(message, data, GlobalHttpStatusCode.INTERNAL_ERROR.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.INTERNAL_ERROR.getCode(), data);
     }
 
     public static <T> ResponseWrapper<T> badRequest(String message) {
-        return new ResponseWrapper<>(message, null, GlobalHttpStatusCode.BAD_REQUEST.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.BAD_REQUEST.getCode(), null);
     }
 
     public static <T> ResponseWrapper<T> badRequest(T data, String message) {
-        return new ResponseWrapper<>(message, data, GlobalHttpStatusCode.BAD_REQUEST.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.BAD_REQUEST.getCode(), data);
     }
 
     public static <T> ResponseWrapper<T> unauthorized(String message) {
-        return new ResponseWrapper<>(message, null, GlobalHttpStatusCode.UNAUTHORIZED.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.UNAUTHORIZED.getCode(), null);
     }
 
     public static <T> ResponseWrapper<T> unauthorized(T data, String message) {
-        return new ResponseWrapper<>(message, data, GlobalHttpStatusCode.UNAUTHORIZED.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.UNAUTHORIZED.getCode(), data);
     }
 
     public static <T> ResponseWrapper<T> forbidden(String message) {
-        return new ResponseWrapper<>(message, null, GlobalHttpStatusCode.FORBIDDEN.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.FORBIDDEN.getCode(), null);
     }
 
     public static <T> ResponseWrapper<T> forbidden(T data, String message) {
-        return new ResponseWrapper<>(message, data, GlobalHttpStatusCode.FORBIDDEN.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.FORBIDDEN.getCode(), data);
     }
 
     public static <T> ResponseWrapper<T> notFound(String message) {
-        return new ResponseWrapper<>(message, null, GlobalHttpStatusCode.NOT_FOUND.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.NOT_FOUND.getCode(), null);
     }
 
     public static <T> ResponseWrapper<T> notFound(T data, String message) {
-        return new ResponseWrapper<>(message, data, GlobalHttpStatusCode.NOT_FOUND.getCode());
+        return new ResponseWrapper<>(message, GlobalHttpStatusCode.NOT_FOUND.getCode(), data);
     }
 
 }
